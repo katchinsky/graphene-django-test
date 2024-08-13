943b1a9362cee7343b9ddffe396c166f4ac83b11
katchinsky <kate.chuprun@gmail.com>
2024-08-13 22:45:35 +0400

    Set up graphene

--- graphene_user/settings.py
+++ graphene_user/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'graphene_user.secure_user',
+    'graphene_django',
 ]
 
 MIDDLEWARE = [
