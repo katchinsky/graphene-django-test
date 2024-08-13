1aad9e9ddaa7b013cab7bbb8d9dd63d48b15223d
katchinsky <kate.chuprun@gmail.com>
2024-08-14 02:02:22 +0400

    Add custom schema and redefine fields resolver for the introspection query

--- graphene_user/secure_user/schema.py
+++ graphene_user/secure_user/schema.py
@@ -5,6 +5,20 @@ from graphene_django import DjangoObjectType
 User = get_user_model()
 
 
+def should_filter_field(name, context):
+    return name == 'user' and not (context.user.is_authenticated or context.user.has_perm('auth.view_user'))
+
+
+def custom_type_resolver(object_type, info, **kwargs):
+    if object_type.name == "Query":
+        filtered_fields = [
+            (name, value) for name, value in object_type.fields.items() if not should_filter_field(name, info.context)
+        ]
+        return filtered_fields
+
+    return object_type.fields
+
+
 class UserType(DjangoObjectType):
     """
     Single User GraphQL Type based on Django model.
@@ -24,4 +38,11 @@ class Query(graphene.ObjectType):
             return User.objects.get(id=id)
         raise Exception('Cannot query field `user` on type `Query`')
 
-schema = graphene.Schema(query=Query)
+
+class CustomSchema(graphene.Schema):
+    def __init__(self, query):
+        super().__init__(query=query)
+        self.graphql_schema.type_map["__Type"].fields["fields"].resolve = custom_type_resolver
+
+
+schema = CustomSchema(query=Query)
